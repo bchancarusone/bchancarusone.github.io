4d927bdc4d69aa681acf2e3a59f0b67c15b9ba45
Brayden-hz <brayden@hepzibah.ai>
2024-06-16 16:18:28 -0400

    Added a bunch more photos

--- scripts.js
+++ scripts.js
@@ -10,6 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const images = [
         { src: 'photos/01.png', camera: 'Pentax ES w/ Kodak Portra 800', date: '2022/10', location: 'Parry Sound, ON' },
         { src: 'photos/02.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Kodak Gold 200', date: '2023/3/15', location: 'Toronto, ON' },
+        { src: 'photos/03.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Kodak Gold 200', date: '2023/7/9', location: 'New York, NY' },
+        { src: 'photos/04.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Kodak Gold 200', date: '2023/8/21', location: 'Burlington, ON' },
+        { src: 'photos/05.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Fuji Superia 400', date: '2024/3/11', location: 'Toronto, ON' },
+        { src: 'photos/05a.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Cinestill 800T', date: '2024/4/30', location: 'Etobicoke, ON' },
+        { src: 'photos/06.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Cinestill 800T', date: '2024/5/10', location: 'Toronto, ON' },
+        { src: 'photos/06a.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Cinestill 800T', date: '2024/5/10', location: 'Toronto, ON' },
+        { src: 'photos/07.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Fuji Superia 400', date: '2024/5/19', location: 'Hamilton, ON' },
+        { src: 'photos/08.png', camera: 'Olympus Stylus Epic Zoom 80 w/ Fuji Superia 400', date: '2024/5/25', location: 'Toronto, ON' },
+        { src: 'photos/08a.png', camera: 'Sony ICLE-6100', date: '2024/5/31', location: 'Burlington, ON' },
+        { src: 'photos/09.png', camera: 'Canon Powershot SD880 IS', date: '2024/6/13', location: 'Hamilton, ON' },
         // Add more images as needed
     ];
 
